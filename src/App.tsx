4f46bbfa36795d1352bbfd825028001581a98831
Ange Rodriguez <ange.rodriguez@thunderwave.fr>
2021-01-14 01:39:04 +0100

    feat : Structure skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,133 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      Hello woooooorld !!!
+    <div id="react-root">
+    <section className="_9eogI E3X2T">
+      <div></div>
+      <main className="SCxLW  o64aR " role="main">
+        <div className="                     Igw0E   rBNOH          YBx95   ybXk5   vwCYk              MGky5                                                              i0EQd                                   " style={{ minHeight: '100%' }}>
+        <div className="rgFsT  ">
+          <div className="gr27e  ">
+            <h1 className="NXVPg Szr5J  coreSpriteLoggedOutWordmark">Instagram</h1>
+            <div className="EPjEi">
+              <form className="HmktE" id="loginForm" method="post">
+                <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm                                                              kEKum                                                "><div className="-MzZI">
+                  <div className="_9GP1n   ">
+                    <label className="f0n8F FATdn">
+                      <span className="_9nyy2">Num. téléphone, nom d’utilisateur ou e-mail</span>
+                      <input aria-label="Num. téléphone, nom d’utilisateur ou e-mail" aria-required="true" maxLength={75} name="username" type="text" className="_2hvTZ pexuQ zyHYP" value="" />
+                    </label>
+                    <div className="i24fI"></div>
+                  </div>
+                </div>
+                <div className="-MzZI">
+                  <div className="_9GP1n   ">
+                    <label className="f0n8F ">
+                      <span className="_9nyy2">Mot de passe</span>
+                      <input aria-label="Mot de passe" aria-required="true" name="password" type="password" className="_2hvTZ pexuQ zyHYP" value="" />
+                    </label>
+                    <div className="i24fI"></div>
+                  </div>
+                </div>
+                <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm    bkEs3                          CovQj                  jKUp7          DhRcB                                                    ">
+                  <button className="sqdOP  L3NKy   y3zKF     " disabled={false} type="submit">
+                    <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm                                                                                                              ">Connexion</div>
+                  </button>
+                </div>
+                <div className="K-1uj Z7p_S">
+                  <div className="s311c"></div>
+                  <div className="_0tv-g">ou</div>
+                  <div className="s311c"></div>
+                </div>
+                <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm    bkEs3                          CovQj                  jKUp7          DhRcB                                                    ">
+                  <button className="sqdOP yWX7d    y3zKF     " type="button">
+                    <span className=" coreSpriteFacebookIcon AeB99"></span>
+                    <span className="KPnG0">Se connecter avec Facebook</span>
+                  </button>
+                </div>
+              </div>
+              <a className="_2Lks6" href="/accounts/password/reset/" tabIndex={0}>Mot de passe oublié&nbsp;?</a>
+            </form>
+          </div>
+        </div>
+        <div className="gr27e">
+          <div className="_7UhW9   xLCgt      MMzan   _0PwGv         uL8Hv         ">
+            <p className="izU2O ">Vous n’avez pas de compte &nbsp;? <a href="/accounts/emailsignup/" tabIndex={0}><span className="_7UhW9   xLCgt       qyrsm      gtFbE      se6yk        ">Inscrivez-vous</span></a>
+            </p>
+          </div>
+        </div>
+        <div className="APQi1">
+          <p className="b_nGN">Téléchargez l’application.</p>
+          <div className="iNy2T">
+            <a className="z1VUo" href="https://itunes.apple.com/app/instagram/id389801252?pt=428156&amp;ct=igweb.loginPage.badge&amp;mt=8&amp;vt=lo">
+              <img alt="Disponible dans l’App Store" className="Rt8TI " src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_french-fr.png/485fcccb52dc.png" />
+            </a>
+            <a className="z1VUo" href="https://play.google.com/store/apps/details?id=com.instagram.android&amp;referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3D43A17CD4-7147-436D-B677-45C3F65C3BCB%26utm_content%3Dlo%26utm_medium%3Dbadge">
+              <img alt="Disponible dans Google Play" className="Rt8TI " src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_french-fr.png/46aa4b6fd58d.png" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
+  </main>
+  <footer className="_8Rna9  _3Laht pC2e0 " role="contentinfo">
+    <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm               KokQV                                                                                               ">
+      <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm                                                              kEKum                                                ">
+        <div className="                     Igw0E     IwRSH        YBx95   ybXk5    _4EzTm  YlhBV                                                                                                            ">
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="https://about.instagram.com/" rel="nofollow noopener noreferrer" target="_blank">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">À propos</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="https://about.instagram.com/blog/" rel="nofollow noopener noreferrer" target="_blank">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">Blog</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="/about/jobs/">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">Emplois</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="https://help.instagram.com/" rel="nofollow noopener noreferrer" target="_blank">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">Aide</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="/developer/">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">API</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="/legal/privacy/">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">Confidentialité</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="/legal/terms/">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">Conditions</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="/directory/profiles/">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">Comptes principaux</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="/directory/hashtags/">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">Hashtags</div>
+            </a>
+          </div>
+          <div className="                     Igw0E     IwRSH      eGOV_         _4EzTm     _22l1                 soMvl                  JI_ht                                                                      ">
+            <a href="/explore/locations/">
+              <div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">Lieux</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="                     Igw0E     IwRSH        YBx95   ybXk5    _4EzTm     _22l1                                                      _49XvD                                                   ">
+        <div className="_7UhW9  PIoXz       MMzan  KV-D4          uL8Hv         "><span className="_3G4x7   RFk-t bwz9f "><div className="TQUPK"><span>Français</span><div className="                     Igw0E     IwRSH      eGOV_         _4EzTm                     WKY0a                                                                                         "><span style={{ display: 'inline-block', transform: 'rotate(180deg)' }}><svg aria-label="Icône chevron vers le bas" className="_8-yf5 " fill="#8e8e8e" height="12" viewBox="0 0 48 48" width="12"><path d="M40 33.5c-.4 0-.8-.1-1.1-.4L24 18.1l-14.9 15c-.6.6-1.5.6-2.1 0s-.6-1.5 0-2.1l16-16c.6-.6 1.5-.6 2.1 0l16 16c.6.6.6 1.5 0 2.1-.3.3-.7.4-1.1.4z"></path></svg></span></div></div><select aria-label="Changer la langue d’affichage" className="hztqj"><option value="af">Afrikaans</option><option value="cs">Čeština</option><option value="da">Dansk</option><option value="de">Deutsch</option><option value="el">Ελληνικά</option><option value="en">English</option><option value="en-gb">English (UK)</option><option value="es">Español (España)</option><option value="es-la">Español</option><option value="fi">Suomi</option><option value="fr">Français</option><option value="id">Bahasa Indonesia</option><option value="it">Italiano</option><option value="ja">日本語</option><option value="ko">한국어</option><option value="ms">Bahasa Melayu</option><option value="nb">Norsk</option><option value="nl">Nederlands</option><option value="pl">Polski</option><option value="pt-br">Português (Brasil)</option><option value="pt">Português (Portugal)</option><option value="ru">Русский</option><option value="sv">Svenska</option><option value="th">ภาษาไทย</option><option value="tl">Filipino</option><option value="tr">Türkçe</option><option value="zh-cn">中文(简体)</option><option value="zh-tw">中文(台灣)</option><option value="bn">বাংলা</option><option value="gu">ગુજરાતી</option><option value="hi">हिन्दी</option><option value="hr">Hrvatski</option><option value="hu">Magyar</option><option value="kn">ಕನ್ನಡ</option><option value="ml">മലയാളം</option><option value="mr">मराठी</option><option value="ne">नेपाली</option><option value="pa">ਪੰਜਾਬੀ</option><option value="si">සිංහල</option><option value="sk">Slovenčina</option><option value="ta">தமிழ்</option><option value="te">తెలుగు</option><option value="vi">Tiếng Việt</option><option value="zh-hk">中文(香港)</option><option value="bg">Български</option><option value="fr-ca">Français (Canada)</option><option value="ro">Română</option><option value="sr">Српски</option><option value="uk">Українська</option></select></span></div><div className="                     Igw0E     IwRSH      eGOV_         _4EzTm                        _5VUwz                                                                                      "><div className="_7UhW9  PIoXz       MMzan   _0PwGv         uL8Hv         ">© 2021 Instagram par Facebook</div></div></div></div></footer></section></div>
   )
 }
 
